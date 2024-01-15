b3cab0f642e5401f65f1558657148fcff1d2970a
Simone-Corso <scorso26@gmail.com>
2024-01-15 13:13:01 +0100

    ora stamp nel url

--- snack-2/snack2.php
+++ snack-2/snack2.php
@@ -1,17 +1,11 @@
 <!--Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
 che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare 
-"Accesso riuscito", altrimenti "Accesso negato"!-->$_COOKIE
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack-2</title>
-</head>
-<body>
+"Accesso riuscito", altrimenti "Accesso negato"!-->
+
 
 <!-- inseriamo il form get!-->
-    <form action="verifica_accesso.php" method="GET">
+
+    <form action="./snack2.php" method="GET">
         <label for="name" class="for">Nome:</label>
         <input type="text" name="name" id="name" required>
 
@@ -28,5 +22,5 @@ che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocci
 
 
     </form>
-</body>
-</html>
+
+
