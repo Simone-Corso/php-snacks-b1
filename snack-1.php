111279dbe5f1d5a1e0bc0bd02a86e632e5c5e0d5
Simone-Corso <scorso26@gmail.com>
2024-01-15 12:18:45 +0100

    ho insetito la prima partita nel array

---
+++ snack-1.php
@@ -0,0 +1,15 @@
+//Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. 
+//Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+//Stampiamo a schermo tutte le partite con questo schema:
+
+//Olimpia Milano - Cantù | 55-60
+
+<?php
+
+//Andiamo a realizzare un array delle partite di basket
+
+$partite_di_basket = [
+    ["ospite" => "Roma", "casa" => "Firenze", "punti_ospite" => 87, "punti_casa" => 90]
+]
+
+    ?>
