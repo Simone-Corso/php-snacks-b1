785ab76a7e5ff1afa8933f27b7875b822ed2f16e
Simone-Corso <scorso26@gmail.com>
2024-01-25 12:30:34 +0100

    ho aggiunto la parte dell'esercizio in alto nel file php

---
+++ OOP/index.php
@@ -0,0 +1,15 @@
+//Create una nuova classe che rappresenti un Pokemon e che abbia almeno cinque proprieta, tre metodi ed il costruttore.
+Microbonus: creo un'altra classe che aggiungo al pokemon per composizione che configuri il tipo di pokemon (aria, terra, ecc.)//
+
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP</title>
+</head>
+<body>
+    
+</body>
+</html>
